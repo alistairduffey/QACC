1898428c4e87397ff264f3dac38be0c8e9b158a2
Alistair Duffey <a_duffey@sci-vm-01.jasmin.ac.uk>
2025-09-25 12:14:40 +0100

    working v2, ARISE only

--- utils/.ipynb_checkpoints/config-checkpoint.py
+++ utils/.ipynb_checkpoints/config-checkpoint.py
@@ -2,7 +2,9 @@
 
 ##### SETTINGS
 model = 'UKESM1-0-LL'
-min_lat, max_lat = 66, 90
+min_lat_arctic, max_lat_arctic = 66, 90
+min_lat_tropics, max_lat_tropics = -23, 23
 min_year_late_cent, max_year_late_cent = "2050", "2070"
+min_year_baseline, max_year_baseline = "2015", "2030"
 
 do_preprocess = True
